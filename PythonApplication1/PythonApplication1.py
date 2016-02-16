66ca8954101751cda98db394bf97eec70f6e9c39
Toni Fairbanks <tfairbanks@patrickco.com>
2016-02-16 12:48:00 -0500

    added utility code

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,11 +1,28 @@
 """
 -------------------------------------------------------------------------------
-Name:        arcpy_util.py
-Purpose:     arcpy utilities
+Name:        PythonApplication1.py
+Purpose:     Sandbox development
 
 Author:      Toni Fairbanks
 
-Created:     8/25/15
+Created:     2/9/2016
 Copyright:   (c) Patrick Engineering 2015
 -------------------------------------------------------------------------------
 """
+
+import arcpy
+
+import collections
+
+def rows_as_dicts(cursor):
+    """ returns rows as dicts (does not maintain field order) """
+    colnames = cursor.fields
+    for row in cursor:
+        yield dict(zip(colnames, row))
+
+
+def rows_as_ordered_dicts(cursor):
+    """ returns rows as collections.OrderedDict """
+    colnames = cursor.fields
+    for row in cursor:
+        yield collections.OrderedDict(zip(colnames, row))
